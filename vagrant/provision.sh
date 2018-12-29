cf9c33259d8de6ae9da1a09a670fbe16c18557f1
Jairo Velasco <jairo.velasco@alertlogic.com>
2018-12-29 11:55:26 -0500

    Provision Docker with TLS enabled

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -4,4 +4,5 @@ cd ../ansible
 VG=../vagrant/.vagrant
 AN=../ansible
 AN_INVENTORY="$VG/provisioners/ansible/inventory/vagrant_ansible_inventory"
+ansible-galaxy install -r requirements.yml
 ansible-playbook --become -f 1 -i "$AN_INVENTORY" "$AN/docker-swarm.yml"
